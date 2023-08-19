069e4e770b303c81b56fb63255a0daea13092449
Amr Elaraby <amrselaraby@gmail.com>
2023-08-19 20:41:56 +0300

    ch04 finished messages

--- account/views.py
+++ account/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from .models import Profile
+from django.contrib import messages
 
 
 def user_login(request):
@@ -27,3 +30,52 @@ def user_login(request):
             "form": form,
         }
     return render(request, "account/login.html", context)
+
+
+@login_required
+def dashboard(request):
+    context = {"section": "dashboard"}
+    return render(request, "account/dashboard.html", context)
+
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(user_form.cleaned_data["password"])
+            # Save the user object
+            new_user.save()
+            Profile.objects.create(user=new_user)
+            return render(request, "account/register_done.html", {"new_user": new_user})
+    else:
+        user_form = UserRegistrationForm()
+
+    return render(request, "account/register.html", {"user_form": user_form})
+
+
+@login_required
+def edit(request):
+    if request.method == "POST":
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(
+            instance=request.user.profile,
+            data=request.POST,
+            files=request.FILES,
+        )
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, "Profile Updated Successfully")
+        else:
+            messages.error(request, "Error updating your profile.")
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    context = {
+        "user_form": user_form,
+        "profile_form": profile_form,
+    }
+    return render(request, "account/edit.html", context)
